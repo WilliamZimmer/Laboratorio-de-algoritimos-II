bb07cda51590a408d3c8efb4331df1482d720ecd
William Zimmer <127452380+WilliamZimmer@users.noreply.github.com>
2023-08-22 19:26:19 -0300

    Update exercicio-II.py

--- semana-2/exercicio-II.py
+++ semana-2/exercicio-II.py
@@ -1,20 +1,19 @@
 def encontrar_duplicatas_e_unificar(lista):
-    duplicados = [] 
-    unificados = [] 
-    contagem = {} 
+    duplicados = []
+    unificados = []
+    elementos_vistos = []
     
     for elemento in lista:
-        if elemento in contagem:
-            contagem[elemento] += 1
-            if contagem[elemento] == 2:
-                duplicados.append(elemento)
+        if elemento in elementos_vistos:
+            duplicados.append(elemento)
         else:
-            contagem[elemento] = 1
+            elementos_vistos.append(elemento)
             unificados.append(elemento)
     
     return duplicados, unificados
 
+# Exemplo de uso
 duplicatas = [1, 2, 2, 3, 4, 4, 5, 6, 6]
 duplicados, unificados = encontrar_duplicatas_e_unificar(duplicatas)
-print("Elementos duplicados:", duplicados)  
-print("Elementos unificados:", unificados)  
+print("Elementos duplicados:", duplicados)
+print("Elementos unificados:", unificados)
