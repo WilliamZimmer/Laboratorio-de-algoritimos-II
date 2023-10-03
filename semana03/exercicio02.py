d119b5c44598c20e02f005efb8ead16c782f7ab6
William Zimmer <127452380+WilliamZimmer@users.noreply.github.com>
2023-10-03 16:38:22 -0300

    Update exercicio02.py

--- semana03/exercicio02.py
+++ semana03/exercicio02.py
@@ -1,20 +1,18 @@
-def calcular_diagonal_inferior(matriz):
+def x(matriz):
     soma = 0
-    for i in range(len(matriz)):
-        for j in range(i+1, len(matriz)):
-            soma += matriz[i][j]
-    return soma
+    n = len(matriz)
 
+    for i in range(n):
+        for j in range(i):
+            soma += matriz[i][j]
+
+    return soma
 
 matriz = [
     [1, 2, 3],
-    [5, 6, 7],
-    [9, 10, 11],
-
+    [4, 5, 6],
+    [7, 8, 9]
 ]
 
-soma_diagonal_inferior = calcular_diagonal_inferior(matriz)
-print("Matriz:")
-for linha in matriz:
-    print(linha)
-print("Soma abaixo da diagonal principal:", soma_diagonal_inferior)
+result = x(matriz)
+print("A soma dos elementos é:", result)
